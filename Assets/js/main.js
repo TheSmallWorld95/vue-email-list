a5129c8e98964899edfa33155162761f707d16db
Fabio-Colombo <thesmallworld95@gmail.com>
2021-04-08 17:19:08 +0200

    Completo con Bonus e sistemato

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -6,13 +6,13 @@ var app = new Vue (
     },
     methods: {
       generate: function () {
-        for (var i = 0; i < 10; i++) {
-          axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-          .then((response) => {
-            let mail = response.data.response;
-            this.mails.push(mail);
-          })
-        }
+          for (var i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+              let mail = response.data.response;
+              this.mails.push(mail);
+            })
+          }
       },
       remove: function () {
         this.mails = []
