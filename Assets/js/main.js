90f4a684b5dfe5d3d84cc8fee1206e1061f8ba94
Fabio-Colombo <thesmallworld95@gmail.com>
2021-04-08 16:44:54 +0200

    Completo

---
+++ Assets/js/main.js
@@ -0,0 +1,20 @@
+var app = new Vue (
+  {
+    el: "#root",
+    data:{
+      mails: [],
+    },
+    methods: {
+      generate: function () {
+        for (var i = 0; i < 10; i++) {
+          axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((response) => {
+            let mail = response.data.response;
+            this.mails.push(mail)
+          })
+
+        }
+      }
+    }
+  }
+)
